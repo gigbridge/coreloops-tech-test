2ffcc6d003b04aa57248f6289c8772222842de6a
sahmmie <smarsatto@gmail.com>
2025-08-28 16:05:40 +0100

    Remove MoveModule and MoveRepository files to streamline move management

--- apps/api/src/app/repositories/moves/move.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MoveRepository } from 'src/app/repositories/moves/move.repository';
-
-@Module({
-  exports: [MoveRepository],
-  providers: [MoveRepository],
-})
-export class MoveModule {}
